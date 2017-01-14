d7c43cd0404e22a6be18c1e079f3f4f469d53298
Pramodh <satyapramodh@gmail.com>
2017-01-14 14:05:34 -0700

    testing waterfall

--- app.js
+++ app.js
@@ -3,16 +3,22 @@ var builder = require('botbuilder');
 
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
+server.listen(process.env.port || 3978, function () {
    console.log('%s listening to %s', server.name, server.url);
 });
 
 // Create bot and add dialogs
 // Create chat bot
+
+// Using powershell:
+// $env:APPID='APPID'
+
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: process.env.APPID,
+    appPassword: process.env.APPPASSWORD
 });
+
+// var connector = new builder.ConsoleConnector().listen();
 
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
@@ -21,6 +27,21 @@ server.post('/api/messages', connector.listen());
 // Bots Dialogs
 //=========================================================
 
-bot.dialog('/', function (session) {
-    session.send("Hello World");
-});
+// bot.dialog('/', function (session) {
+//     session.send('Hello World');
+// });
+
+bot.dialog('/', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        console.log(results);
+        session.send('Hello %s!', results.response);
+        builder.Prompts.text(session, 'Hi! What is your age?');
+    },
+    function (session, results) {
+      console.log(results);
+      session.send('%s ? you are old!', results.response);
+    }
+]);
