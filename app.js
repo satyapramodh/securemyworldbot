a71f84894444b205c0174e948943bf0d0a28a40e
Pramodh <satyapramodh@gmail.com>
2017-01-14 19:53:05 -0700

    intent usage

--- app.js
+++ app.js
@@ -23,62 +23,47 @@ var connector = new builder.ChatConnector({
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
+
+var intents = new builder.IntentDialog();
+bot.dialog('/', intents);
+
 //=========================================================
 // Bots Dialogs
 //=========================================================
 
-// bot.dialog('/', function (session) {
-//     session.send('Hello World');
-// });
-
-// bot.dialog('/', [
-//     function (session) {
-//         builder.Prompts.text(session, 'Hi! What is your name?');
-//     },
-//     function (session, results) {
-//         console.log(results);
-//         session.send('Hello %s!', results.response);
-//         builder.Prompts.text(session, 'Hi! What is your age?');
-//     },
-//     function (session, results) {
-//       console.log(results);
-//       session.send('%s ? you are old!', results.response);
-//     }
-// ]);
-
-bot.dialog('/',[
-  function (session, args, next) {
-    if(!session.userData.id || !session.userData.name){
-      session.beginDialog('/profile');
-
-    } else {
-      next();
+intents.matches(/^change name/i, [
+    function (session) {
+        session.beginDialog('/profile');
+    },
+    function (session, results) {
+        session.send('Ok... Changed your name to %s', session.userData.name);
     }
-  },
-  function (session) {
-    session.send("Welcome %s", session.userData.name);
-  }
+]);
+
+intents.onDefault([
+    function (session, args, next) {
+        if (!session.userData.name) {
+            session.beginDialog('/profile');
+        } else {
+            next();
+        }
+    },
+    function (session, results) {
+        session.send('Hello %s!', session.userData.name);
+    }
 ]);
 
 bot.dialog('/profile', [
-  function (session, args, next) {
-    if(!session.userData.name){
-      builder.Prompts.text(session, "Hey! Whats your name?");
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
     }
-  },
-  function (session, results) {
-    if(results && results.response){
-      session.userData.name = results.response;
-    }
-    if(!session.userData.id){
-      builder.Prompts.text(session, "So Whats your unique id?");
-    }
-  },
-  function (session, results) {
-    if(results && results.response){
-      session.userData.id = results.response;
-    }
-    // end dialog and get back to caller
-    session.endDialog();
-  }
-])
+]);
+
+// var msg = new builder.Message()
+//                 .address(alarm.address)
+//                 .text("Here's your '%s' alarm.", alarm.title);
+//             bot.send(msg);
